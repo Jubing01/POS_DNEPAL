9b9d3d96320f3f30bcc2e57f5e10b3489ddd67fc
Asim Poudel <bipulp49@gmail.com>
2025-11-25 10:39:48 +0545

    feat(sidebar): added sidebar in admin for easier access to pages.

---
+++ components/inventory-app-sidebar.tsx
@@ -0,0 +1,49 @@
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import Link from "next/link";
+
+import { Package } from "lucide-react";
+
+const inventoryItems = [
+  {
+    title: "Product",
+    url: "/admin/product",
+    icon: Package,
+  },
+];
+
+const InventorySectionSidebar = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Inventory</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {inventoryItems.map((item) => {
+            return (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <Link
+                    href={item.url}
+                    className="flex items-center gap-2 w-full"
+                  >
+                    <item.icon size={16} />
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
+
+export default InventorySectionSidebar;
