2e2e48bb89c6d7c1c1a300bd1b1a8c4bbad03607
Jubing01 <np03cs4s220194@heraldcollege.edu.np>
2025-12-03 12:16:50 +0545

    feat(authentication): worked with user interaction on resetnewpassword page and forgotpass page

--- app/login/forgotpass/page.tsx
+++ app/login/forgotpass/page.tsx
@@ -6,6 +6,10 @@ import { useState } from "react";
 export default function ForgotPassword() {
 
     const [email,setEmail] = useState("")
+    const handleLogin = (event) => {
+        event.preventDefault()
+        // console.log(email)
+    }
 
     return(
         <div className="min-h-screen flex items-center justify-center bg-gray-300">
@@ -16,28 +20,26 @@ export default function ForgotPassword() {
                     Enter your email and we will send you a password reset link.
                 </p>
 
-                <form className="space-y-5">
+                <form className="space-y-5" onSubmit={handleLogin}>
 
                     <div className="flex flex-col space-y-2">
                         <label className="text-sm font-medium text-gray-700"> Email </label>
                         <input 
                             type="email" 
                             name="email"
-                            placeholder="Timro email type gara ta" required
+                            placeholder="Timro email type gara ta" required onChange={(event) => setEmail(event.target.value)}
                             className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
                     </div>
 
                     <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition ">
                         Send Reset Link
                     </button>
-
                 </form>
 
                 <div className="text-center mt-4">
                     <a href="/login" className="text-blue-600 text-sm hover:underline"> Login  </a>
 
                 </div>
-
             </div>
         </div>
     );
--- app/login/resetnewpassword/page.tsx
+++ app/login/resetnewpassword/page.tsx
@@ -1,5 +1,18 @@
 //@ts-nocheck
+'use client'
+
+import { useState } from "react"
+
 export default function (){
+    
+    const [Npassword, setNpassword] = useState("")
+    const [Cpassword, setCpassword] = useState("")
+
+    const handleLogin = event => {
+        event.preventDefault()
+        console.log(Npassword, Cpassword)
+    }
+
     return(
             <div className="min-h-screen flex items-center justify-center bg-gray-300">
                 <div className="bg-white p-8 rounded-xl shadow-lg w-96">
@@ -10,47 +23,40 @@ export default function (){
                     Enter your new password below.
                 </p>
 
-                <form className="space-y-5">
+                <form className="space-y-5" onSubmit={handleLogin}>
 
                     <div className="flex flex-col space-y-2">
-                        <label className="text-sm font-medium text-gray-700">
-                            New Password
-                        </label>
+                        <label className="text-sm font-medium text-gray-700"> New Password </label>
                         <input
                             type="password"
                             name="new_password"
                             placeholder="Enter new password"
-                            required
+                            required onChange={(event) => setNpassword(event.target.value)}
                             className="border border-gray-400 rounded-lg px-3 py-2 
                                     focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
 
                     <div className="flex flex-col space-y-2">
-                        <label className="text-sm font-medium text-gray-700">
-                            Confirm Password
-                        </label>
+                        <label className="text-sm font-medium text-gray-700"> Confirm Password </label>
                         <input
                             type="password"
                             name="confirm_password"
                             placeholder="Confirm your password"
-                            required
+                            required onChange={(event) => setCpassword(event.target.value)}
                             className="border border-gray-400 rounded-lg px-3 py-2 
-                                    focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
+                                    focus:outline-none focus:ring-2 focus:ring-blue-500"/>
                     </div>
 
                     <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
                         Reset Password
                     </button>
-
                 </form>
 
                 <div className="text-center mt-4">
                     <a href="/login" className="text-blue-600 text-sm hover:underline">Back to Login</a>
                 </div>
             </div>
-
             </div>
     )
 }
