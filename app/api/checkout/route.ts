a6b4383c712a6fb13219d89d0a0d10774dac78f7
Asim Poudel <bipulp49@gmail.com>
2026-04-24 12:10:10 +0545

    feat(add invoice and billing): add invoice printout and sales and sale item add in database

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,3 +1,6 @@
+import { Product, User } from "@/app/generated/prisma/client";
+import { verifyAuth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
@@ -5,9 +8,77 @@ export async function POST(
   { params }: { params: Promise<{ id: string }> },
 ) {
   const body = await request.json();
-  console.log("body", body);
+  const user: User = await verifyAuth();
+  if (!user.companyId) {
+    return NextResponse.json({
+      success: false,
+      message: "Company user of a specific company can checkout",
+    });
+  }
+  const items = await prisma.sale.create({
+    data: {
+      userId: user.id,
+      companyId: user.companyId,
+      subtotal: body.subtotal,
+      discount: body.discount,
+      vat: body.vat,
+      total: body.total,
+      status: "PENDING",
+
+      items: {
+        create: body.cart.map(
+          (item: { product: Product; quantity: number }) => ({
+            productId: item.product.id,
+            quantity: item.quantity,
+            price: item.product.sellingPrice,
+            total: item.product.sellingPrice * item.quantity,
+            companyId: user.companyId,
+          }),
+        ),
+      },
+    },
+    include: {
+      items: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  // Get company details
+  const company = await prisma.company.findUnique({
+    where: { id: user.companyId },
+  });
+
   return NextResponse.json({
     success: true,
     message: "Checkout Carried Successfully",
+    data: {
+      sale: items,
+      company,
+      user,
+    },
+  });
+}
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const sales = await prisma.sale.findMany({
+    include: {
+      items: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json({
+    success: true,
+    message: "All checkouts",
+    sales,
   });
 }
